85c106cc11734e738b26efcc6d2d0662b3aa02cb
Barapla <araujobryan333@gmail.com>
2025-06-29 03:08:33 -0600

    add the home

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Home controller
+class HomeController < ApplicationController
+  # This controller serves the home page of the application.
+  # It inherits from ApplicationController, which handles common functionality like authentication.
+
+  def index
+    # The index action can be used to render the home page view.
+    # Additional logic can be added here if needed.
+  end
+end
